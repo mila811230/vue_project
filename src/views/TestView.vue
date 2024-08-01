8e08864e196bbf66d1b7ecb72fdc9a1a0729cd9d
mila811230 <bluegalaxy1230@gmail.com>
2024-08-01 10:40:35 +0900

    0801vuebuild

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,31 +1,15 @@
 <template>
   <div class="">
-    
-        <input type="checkbox" id = "cbtn_agree" name = "cbtn_agree">
-        <label for="cbtn_agree">동의</label><br>
+    <h3>--------------------------------속성연결-------------------------------- </h3>
+    <img src='https://borgssam.github.io/MySite/img/album_01.jpg' title ="가방을 든 사나이"><br>
+    <img v-bind:src="imgSrc" alt="" v-bind:title = "imgTitle"><br>
+  </div>
 
-        <label><input type="checkbox" v-model = "agree">동의</label><br>
-        <label><input type="checkbox" v-model = "agree2"
-            true-value = "동의" false-value = "비동의">{{agree2}}</label><br>
-        
-        <div>
-        <p>기본 피자에 원하시는 토핑을 추가하세요.</p>
-            <label><input type="checkbox" v-model = "pizza" value = "치즈">치즈</label><br>
-            <label><input type="checkbox" v-model = "pizza" value = "파인애플">파인애플</label><br>
-            <label><input type="checkbox" v-model = "pizza" value = "버섯">버섯</label><br>
-        <p>당신이 추가하신 토핑은 {{pizza}}입니다</p>
-        </div>
+  <br>
 
-        <br>
-
-        <div>
-            <p>당신의 탕수육 취향은?</p>
-            <label><input type ="radio" v-model = "sweet_sour_pork" value = "부먹">부먹</label>
-            <label><input type ="radio" v-model = "sweet_sour_pork" value = "찍먹">찍먹</label>
-            <!-- <label><input type ="radio" v-model = "sweet_sour_pork" value = "찍먹">처먹</label> -->
-            <p>당신은 <span style="color:red; font-weight: 800;">{{ sweet_sour_pork }}파 입니다.</span></p>
-        </div>
-
+  <div>
+    <button :disabled = "btn_01" :style = "color_blue">버튼</button>
+    <button :disabled = "btn_02" :style = "color_red">버튼</button>
   </div>
 </template>
 
@@ -37,10 +21,12 @@ export default {
   },
   data() {
     return {
-        agree: false,
-        agree2: '비동의',
-        pizza: [],
-        sweet_sour_pork: '찍먹',
+        imgSrc: 'https://borgssam.github.io/MySite/img/album_01.jpg',
+        imgTitle: '가방을 든 사나이',
+        btn_01: true,
+        btn_02: false,
+        color_blue: {color: 'blue', fontSize: '24px'},
+        color_red: {color: 'red', fontSize: '24px'},
       
     }
   },
